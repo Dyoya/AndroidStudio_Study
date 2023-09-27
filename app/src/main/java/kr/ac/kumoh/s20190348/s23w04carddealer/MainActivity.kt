c1b315d6275d064fbee06a1d8210b41092f86326
khm05 <khm0509@naver.com>
2023-09-27 16:21:46 +0900

    😎 Init Setting

--- app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.ac.kumoh.s20190348.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20190348.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
     }
 }
