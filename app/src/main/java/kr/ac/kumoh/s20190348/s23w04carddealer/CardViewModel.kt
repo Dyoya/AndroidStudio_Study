49d02ff80f5f94f1706687bde3d40bc1b55a27ad
Dyoya <khm0509@naver.com>
2023-10-11 16:19:58 +0900

    😎 카드 랜덤 출력

--- app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/CardViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/CardViewModel.kt
@@ -2,22 +2,27 @@ package kr.ac.kumoh.s20190348.s23w04carddealer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class CardViewModel {
-    private val _cards = MutableLiveData<IntArray>(IntArray(5) { 0 })
+class CardViewModel : ViewModel() {
+    private val _cards = MutableLiveData<IntArray>(IntArray(5) { -1 })
     val cards: LiveData<IntArray>
         get() = _cards
 
-    fun generate() {
+    fun shuffle() {
         var num = 0
-        var newCards = IntArray(5) { 0 }
+        var newCards = IntArray(5) { -1 }
         for (i in newCards.indices) {
             // 중복 검사
             do {
-                num = Random.nextInt(1, 46)
+                num = Random.nextInt(52)
             } while (newCards.contains(num))
             newCards[i] = num
         }
+
+        newCards.sort()
+
+        _cards.value = newCards
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/s23w04carddealer/MainActivity.kt
@@ -3,36 +3,47 @@ package kr.ac.kumoh.s20190348.s23w04carddealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.s20190348.s23w04carddealer.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
+    private lateinit var model: CardViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(main.root)
 
-        val card = Random.nextInt(52)
-        Log.i("Card!!", "$card : ${getCardName(card)}")
+        model = ViewModelProvider(this)[CardViewModel::class.java]
+        model.cards.observe(this, Observer {
+            val res = IntArray(5)
+            for(i in it.indices) {
+                    res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
 
-        val res = resources.getIdentifier(
-            getCardName(card),
-            "drawable",
-            packageName
-        )
+            main.card1.setImageResource(res[0])
+            main.card2.setImageResource(res[1])
+            main.card3.setImageResource(res[2])
+            main.card4.setImageResource(res[3])
+            main.card5.setImageResource(res[4])
+        })
 
-        // TODO : 카드 랜덤으로 출력되도록
-        main.card1.setImageResource(res)
-        main.card2.setImageResource(R.drawable.c_ace_of_hearts)
-        main.card3.setImageResource(R.drawable.c_ace_of_hearts)
-        main.card4.setImageResource(R.drawable.c_ace_of_hearts)
-        main.card5.setImageResource(R.drawable.c_ace_of_hearts)
-
+        main.btn.setOnClickListener {
+            model.shuffle()
+            // TODO : 족보 출력하기
+            main.text.setText("족보 출력")
+        }
     }
 
     private fun getCardName(c: Int) : String {
-        val shape = when (c / 13) {
+        val shape = when (c % 4) {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
@@ -40,15 +51,18 @@ class MainActivity : AppCompatActivity() {
             else -> "error"
         }
 
-        val number = when (c % 13) {
+        val number = when (c / 4) {
             0 -> "ace"
-            in 1..9 -> (c % 13 + 1).toString()
+            in 1..9 -> (c / 4 + 1).toString()
             10 -> "jack"
             11 -> "queen"
             12 -> "king"
             else -> "error"
         }
 
-        return "c_${number}_of_${shape}"
+        if(c == -1)
+            return "c_red_joker"
+        else
+            return "c_${number}_of_${shape}"
     }
 }
