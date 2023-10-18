2b43d545a43afc2bb6a79ad8900b00208b9c971d
Dyoya <khm0509@naver.com>
2023-10-18 16:35:42 +0900

    ImageActivity

--- app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/ImageActivity.kt
@@ -2,10 +2,22 @@ package kr.ac.kumoh.s20190348.a23w07intentdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kr.ac.kumoh.s20190348.a23w07intentdata.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
+    private lateinit var main: ActivityImageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_image)
+
+        //Log.i("ImageActivity!!", intent.getStringExtra("location") ?: "없음")
+        val res = when(intent.getStringExtra(MainActivity.KEY_NAME)) {
+            "mountain" -> R.drawable.mountain
+            "sea" -> R.drawable.sea
+            else -> R.drawable.ic_launcher_foreground
+        }
+        main.image.setImageResource(res)
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/MainActivity.kt
@@ -1,23 +1,33 @@
 package kr.ac.kumoh.s20190348.a23w07intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kr.ac.kumoh.s20190348.a23w07intentdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{ // static
+        const val KEY_NAME = "location"
+    }
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(main.root)
         
         main.btnMountain.setOnClickListener {
             Toast.makeText(this, "산 눌림", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ImageActivity::class.java)
+            intent.putExtra(KEY_NAME, "mountain")
+            startActivity(intent)
         }
 
         main.btnSea.setOnClickListener {
             Toast.makeText(this, "바다 눌림", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ImageActivity::class.java)
+            intent.putExtra(KEY_NAME, "sea")
+            startActivity(intent)
         }
     }
 }
