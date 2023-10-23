bb92ce3e87d16ac8150afe4e0f54e908b84a4665
Dyoya <khm0509@naver.com>
2023-10-23 15:50:26 +0900

    🥞 onClick

--- app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/ImageActivity.kt
@@ -1,23 +1,50 @@
 package kr.ac.kumoh.s20190348.a23w07intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import com.google.android.material.color.utilities.DislikeAnalyzer
 import kr.ac.kumoh.s20190348.a23w07intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity(),OnClickListener {
+    companion object {
+        const val IMAGE_NAME = "image name"
+        const val IMAGE_RESULT = "image result"
+
+        const val LIKE = 100
+        const val DISLIKE = 101
+        const val NONE = 0
+    }
     private lateinit var main: ActivityImageBinding
+    private lateinit var location: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityImageBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_image)
+        setContentView(main.root)
 
         //Log.i("ImageActivity!!", intent.getStringExtra("location") ?: "없음")
-        val res = when(intent.getStringExtra(MainActivity.KEY_NAME)) {
-            "mountain" -> R.drawable.mountain
-            "sea" -> R.drawable.sea
+        location = intent.getStringExtra(MainActivity.KEY_NAME) ?: "none"
+        val res = when (location) {
+            MainActivity.MOUNTAIN -> R.drawable.mountain
+            MainActivity.SEA -> R.drawable.sea
             else -> R.drawable.ic_launcher_foreground
         }
         main.image.setImageResource(res)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent()
+        val value = when(p0?.id) {
+            main.btnLike.id -> LIKE
+            main.btnDislike.id -> DISLIKE
+            else -> NONE
+        }
+        intent.putExtra(IMAGE_NAME, location)
+        intent.putExtra(IMAGE_RESULT, value)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190348/a23w07intentdata/MainActivity.kt
@@ -3,12 +3,16 @@ package kr.ac.kumoh.s20190348.a23w07intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import kr.ac.kumoh.s20190348.a23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object{ // static
         const val KEY_NAME = "location"
+        const val MOUNTAIN = "mountain"
+        const val SEA = "sea"
     }
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,18 +20,24 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
         
-        main.btnMountain.setOnClickListener {
-            Toast.makeText(this, "산 눌림", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "mountain")
-            startActivity(intent)
-        }
+        main.btnMountain.setOnClickListener(this)
+        main.btnSea.setOnClickListener(this)
+    }
 
-        main.btnSea.setOnClickListener {
-            Toast.makeText(this, "바다 눌림", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "sea")
-            startActivity(intent)
+    override fun onClick(v: View?) {
+        val intent = Intent(this, ImageActivity::class.java)
+        val value = when (v?.id) {
+            main.btnMountain.id -> {
+                Toast.makeText(this, "산 눌림", Toast.LENGTH_SHORT).show()
+                MOUNTAIN
+            }
+            main.btnSea.id -> {
+                Toast.makeText(this, "바다 눌림", Toast.LENGTH_SHORT).show()
+                SEA
+            }
+            else -> return
         }
+        intent.putExtra(KEY_NAME, value)
+        startActivity(intent)
     }
 }
