1e827151eb53f931db66aa45e623140dda1509d2
Hyeong Min <118094189+Dyoya@users.noreply.github.com>
2023-11-22 16:32:37 +0900

    🎆 Lazy Column

--- S23W1203LazyList/app/src/main/java/kr/ac/kumoh/s20190348/s23w1203lazylist/MainActivity.kt
+++ S23W1203LazyList/app/src/main/java/kr/ac/kumoh/s20190348/s23w1203lazylist/MainActivity.kt
@@ -3,44 +3,75 @@ package kr.ac.kumoh.s20190348.s23w1203lazylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.s20190348.s23w1203lazylist.ui.theme.S23W1203LazyListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            S23W1203LazyListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun MyScreen() {
     S23W1203LazyListTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MyList()
+        }
+    }
+}
+@Composable
+fun TitleText(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+@Composable
+fun SingerText(title: String) {
+    Text(title, fontSize = 20.sp)
+}
+@Composable
+fun SongItem(index: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TitleText(title = "노래 $index")
+        SingerText("이 노래를 부른 가수는 $index 입니다.")
+    }
+}
+@Composable
+fun MyList() {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(40) {
+            SongItem(it)
+        }
     }
 }
